74581dff20a5b8fce5a7f4f7f7fba33100d834ef
deejay94 <dahjoyner12@gmail.com>
2017-10-12 15:30:03 -0400

    Add getBlogs success and failure function
        Add getBlogs success and failure function
        Require handlebars template
        Add function in getBlogsSuccess to show handlebars

--- assets/scripts/contents/ui.js
+++ assets/scripts/contents/ui.js
@@ -1,7 +1,7 @@
 const store = require('../store')
+const showBlogsTemplate = require('../templates/blog-listing.handlebars')
 
 const createContentSuccess = function (data) {
-  store.user = data.user
   $('#message').text('You have succesfully created content!')
   $('#sign-up').hide()
   $('#sign-in').show()
@@ -11,7 +11,20 @@ const createContentFailure = function () {
   $('#message').text('Error on creating content')
 }
 
+const getAllContentSuccess = function (data) {
+  $('#message').text('Success on getting all content')
+  const showBlogsHtml = showBlogsTemplate({ contents: data.contents })
+    $('.showBlogs').empty()
+    $('.showBlogs').append(showBlogsHtml)
+}
+
+const getAllContentFailure = function () {
+  $('#message').text('Error on getting all content')
+}
+
 module.exports = {
   createContentSuccess,
-  createContentFailure
+  createContentFailure,
+  getAllContentFailure,
+  getAllContentSuccess
 }
