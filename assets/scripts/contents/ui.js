5ecc33b77d6d1552ffdb40b41cf249f557cdc9a0
ripleyofvenus <ripleyofvenus@gmail.com>
2017-10-15 16:31:12 -0400

    Fixed .html bug
    For Edit Content (Page & Post): When the data object was prepared to be
    sent to the api, .html was used to reconfigure the data. This created a
    bug where entering &, <, or > would make visible the meta charset value.
    This was fixed by changing .html to .text
    
    Ripley solo

--- assets/scripts/contents/ui.js
+++ assets/scripts/contents/ui.js
@@ -68,10 +68,10 @@ const onEditPost = function (event) {
 }
 
 const onSavePost = function (id, title, date, text, type) {
-  const newTitle = $(title).html()
-  const newText = $(text).html()
-  const newDate = $(date).html().trim()
-  const newType = $(type).html()
+  const newTitle = $(title).text()
+  const newText = $(text).text()
+  const newDate = $(date).text().trim()
+  const newType = $(type).text()
   const data =
 {
   content: {
@@ -126,10 +126,10 @@ const onEditPage = function (event) {
 }
 
 const onSavePage = function (id, title, date, text, type) {
-  const newTitle = $(title).html()
-  const newText = $(text).html()
-  const newDate = $(date).html().trim()
-  const newType = $(type).html()
+  const newTitle = $(title).text()
+  const newText = $(text).text()
+  const newDate = $(date).text().trim()
+  const newType = $(type).text()
   const data =
 {
   content: {
@@ -167,7 +167,6 @@ const getPagesFailure = function () {
 }
 
 const updatePostSuccess = function () {
-  console.log('YEAH BUDDY')
   $('#message').text('Post updated')
   api.getContent()
     .then(getPostsSuccess)
